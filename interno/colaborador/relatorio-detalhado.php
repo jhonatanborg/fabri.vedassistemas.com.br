e04b502219b511fd08610a38f9cfbf17217ea6eb
jhonatanborg <jhonatanborgesdj@gmail.com>
2023-07-17 14:32:27 -0300

    🐛 FIX: report colaborator

--- interno/colaborador/relatorio-detalhado.php
+++ interno/colaborador/relatorio-detalhado.php
@@ -31,88 +31,92 @@ $VarNivel = $_SESSION['s_nivel'];
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <style type="text/css">
-    body {
-        color: #566787;
-        background: #f5f5f5;
-        font-family: 'Varela Round', sans-serif;
-        font-size: 13px;
-    }
+body {
+    color: #566787;
+    background: #f5f5f5;
+    font-family: 'Varela Round', sans-serif;
+    font-size: 13px;
+}
 </style>
 
 <body>
     <!-- Static navbar -->
     <nav class="navbar navbar-default">
 
-<div class="container-fluid">
+        <div class="container-fluid">
 
-  <div class="navbar-header">
+            <div class="navbar-header">
 
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
 
-      <span class="sr-only">Toggle navigation</span>
+                    <span class="sr-only">Toggle navigation</span>
 
-      <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
 
-      <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
 
-      <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
 
-    </button>
+                </button>
 
-    <a class="navbar-brand" href="#">Fabri Gráfica Digital</a>
+                <a class="navbar-brand" href="#">Fabri Gráfica Digital</a>
 
-  </div>
+            </div>
 
-  <div id="navbar" class="navbar-collapse collapse">
+            <div id="navbar" class="navbar-collapse collapse">
 
-    <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav">
 
-      <li><a href="index.php">INICIO</a></li>
-      <li class="active"><a href="relatorio-detalhado.php">Relatório detalhado</a></li>
-    </ul>
+                    <li><a href="index.php">INICIO</a></li>
+                    <li class="active"><a href="relatorio-detalhado.php">Relatório detalhado</a></li>
+                </ul>
 
-    <ul class="nav navbar-nav navbar-right">
+                <ul class="nav navbar-nav navbar-right">
 
-     
 
-     
 
-      <li><a href="#"><?php echo "$VarNome"; ?></a></li>
 
-      <li><a href="../administrador/logout.php">SAIR</a></li>
 
-    </ul>
+                    <li><a href="#"><?php echo "$VarNome"; ?></a></li>
 
-  </div><!--/.nav-collapse -->
+                    <li><a href="../administrador/logout.php">SAIR</a></li>
 
-</div><!--/.container-fluid -->
+                </ul>
 
-</nav>
+            </div>
+            <!--/.nav-collapse -->
+
+        </div>
+        <!--/.container-fluid -->
+
+    </nav>
     <div class="container">
         <h1>Pesquisar</h1>
-        <form method="POST" action="resultado-novo-relatorio.php">
-            <div class="form-group form-group-multiple-selectsp col-xs-11 col-sm-8 col-md-5 mt-6">
+        <form method="POST" class="row" action="resultado-novo-relatorio.php">
+            <div class="form-group form-group-multiple-selectsp col-xs-11 col-sm-8 col-md-5 ">
                 <div class="input-group input-group-multiple-selectp col-xs-8">
                     <select class="form-control" name="ano" required>
                         <option value="">Selecione o Ano</option>
-                      <option value="2019">2019</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    <option value="2025">2025</option>
-                    <option value="2026">2026</option>
-                    <option value="2027">2027</option>
-                    <option value="2028">2028</option>
-                    <option value="2029">2029</option>
-                    <option value="2030">2030</option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                        <option value="2021">2021</option>
+                        <option value="2022">2022</option>
+                        <option value="2023">2023</option>
+                        <option value="2024">2024</option>
+                        <option value="2025">2025</option>
+                        <option value="2026">2026</option>
+                        <option value="2027">2027</option>
+                        <option value="2028">2028</option>
+                        <option value="2029">2029</option>
+                        <option value="2030">2030</option>
                     </select>
 
                 </div>
             </div>
-            <div class="form-group form-group-multiple-selectsp col-xs-11 col-sm-8 col-md-4 mt-6">
+            <div class="form-group form-group-multiple-selectsp col-xs-11 col-sm-8 col-md-4 ">
                 <div class="input-group input-group-multiple-selectp col-xs-8">
+
                     <select class="form-control" name="mes" required>
                         <option value="">Selecione o mês</option>
                         <option value="01">Janeiro</option>
@@ -133,6 +137,16 @@ $VarNivel = $_SESSION['s_nivel'];
 
                 </div>
 
+            </div>
+            <div class="form-group form-group-multiple-selectsp col-xs-11 col-sm-8 col-md-4 ">
+                <div class="input-group">
+                    <label for="">Status</label>
+                    <select class="form-control" name="status">
+                        <option value="all">TODOS</option>
+                        <option value="4">Concluido</option>
+                        <option value="1">Confirmado</option>
+                    </select>
+                </div>
             </div>
             <button type="submit" class="btn btn-danger"> Consultar
             </button>
--- interno/colaborador/resultado-novo-relatorio.php
+++ interno/colaborador/resultado-novo-relatorio.php
@@ -25,7 +25,15 @@ $VarNivel = $_SESSION['s_nivel'];
 
 $mes = $_POST['mes'];
 $ano = $_POST['ano'];
+$status = $_POST['status'];
 
+$statusCondition = "";
+
+if ($status !== "all") {
+  $statusCondition = "AND impressao.status = '$status'";
+} else {
+  $statusCondition = "";
+};
 $month = $_POST['mes']; {
     switch ($month) {
         case "1":
@@ -68,6 +76,49 @@ $month = $_POST['mes']; {
         $month = "erro";
     }
 }
+function formatCurrency($value){
+   // string R$ + number_format
+ return "R$ " . number_format($value, '4', ',','.');
+
+};
+$result_impres = "SELECT
+  impressao.id_unidade,
+  SUM(impressao.quantidade) AS totquantidade,
+  produtos.valor_unidade AS valortotal,
+  SUM(impressao.quantidade * produtos.valor_unidade) AS valortotal,
+  impressao.id_produto,
+  produtos.descricao_prod,
+  produtos.valor_unidade,
+  produtos.codigo,
+  impressao.id_professor,
+  usuarios.nome AS nome_professor,
+  unidades.name as unidade_nome,
+  impressao.status
+FROM impressao
+JOIN produtos ON impressao.id_produto = produtos.id
+JOIN usuarios ON impressao.id_professor = usuarios.id
+JOIN unidades ON usuarios.unidade = unidades.id
+WHERE  EXTRACT(MONTH FROM impressao.data) = '$mes'
+  AND EXTRACT(YEAR FROM impressao.data) = '$ano'
+  AND impressao.status = '4'
+  $statusCondition
+GROUP BY impressao.id_unidade, impressao.id_produto, produtos.descricao_prod, produtos.valor_unidade, produtos.codigo, impressao.id_professor, usuarios.nome
+ORDER BY impressao.id_unidade LIMIT 0,100
+";
+
+$resultado_impres = mysqli_query($conn, $result_impres);
+$total = 0;
+$total_price =0;
+
+function Status($status) {
+	switch($status){
+        case 1:
+            return "CONFIRMADO";
+          
+        case 4:
+            return "CONCLUIDO";
+    }
+}
 ?>
 
 
@@ -82,450 +133,173 @@ $month = $_POST['mes']; {
 
 <head>
 
-	<meta charset="utf-8">
+    <meta charset="utf-8">
 
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Coordenador</title>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<style type="text/css">
-	body {
+    <title>Coordenador</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../administrador/css/style.css">
 
-		color: #566787;
-
-		background: #f5f5f5;
-
-		font-family: 'Varela Round', sans-serif;
-
-		font-size: 13px
-	}
-	.table-wrapper {
-
-		background: #fff;
-
-		padding: 15px 15px;
-
-		margin: 30px 0;
-
-		border-radius: 3px;
-
-		box-shadow: 0 1px 1px rgba(0,0,0,.05);
-
-	}
-
-	.table-title {
-
-		padding-bottom: 15px;
-
-		background: #435d7d;
-
-		color: #fff;
-
-		padding: 16px 30px;
-
-		margin: -20px -25px 10px;
-
-		border-radius: 3px 3px 0 0;
-
-	}
-
-	.table-title h2 {
-
-		margin: 5px 0 0;
-
-		font-size: 24px;
-
-	}
-
-	.table-title .btn-group {
-
-		float: right;
-
-	}
-
-	.table-title .btn {
-
-		color: #fff;
-
-		float: right;
-
-		font-size: 13px;
-
-		border: none;
-
-		min-width: 50px;
-
-		border-radius: 2px;
-
-		border: none;
-
-		outline: none !important;
-
-		margin-left: 10px;
-
-	}
-
-	.table-title .btn i {
-
-		float: left;
-
-		font-size: 21px;
-
-		margin-right: 5px;
-
-	}
-
-	.table-title .btn span {
-
-		float: left;
-
-		margin-top: 2px;
-
-	}
-
-	table.table tr th, table.table tr td {
-
-		border-color: #e9e9e9;
-
-		padding: 12px 15px;
-
-		vertical-align: middle;
-
-	}
-
-	table.table tr th:first-child {
-
-		width: 10px;
-
-	}
-
-	table.table tr th:last-child {
-
-		width: 80px;
-
-	}
-
-	table.table-striped tbody tr:nth-of-type(odd) {
-
-		background-color: #fcfcfc;
-
-	}
-
-	table.table-striped.table-hover tbody tr:hover {
-
-		background: #f5f5f5;
-
-	}
-
-	table.table th i {
-
-		font-size: 13px;
-
-		margin: 0 5px;
-
-		cursor: pointer;
-
-	}
-
-	table.table td:last-child i {
-
-		opacity: 0.9;
-
-		font-size: 22px;
-
-		margin: 0 5px;
-
-	}
-
-	table.table td a {
-
-		font-weight: bold;
-
-		color: #566787;
-
-		display: inline-block;
-
-		text-decoration: none;
-
-		outline: none !important;
-
-	}
-
-	table.table td a:hover {
-
-		color: #2196F3;
-
-	}
-
-	table.table td a.edit {
-
-		color: #FFC107;
-
-	}
-
-	table.table td a.delete {
-
-		color: #F44336;
-
-	}
-
-	table.table td i {
-
-		font-size: 19px;
-
-	}
-
-	table.table .avatar {
-
-		border-radius: 50%;
-
-		vertical-align: middle;
-
-		margin-right: 10px;
-
-	}
-
-
-
-	.hint-text {
-
-		float: left;
-
-		margin-top: 10px;
-
-		font-size: 13px;
-
-	}
-
-
-
-
-
-	/* Modal styles */
-
-	.modal .modal-dialog {
-
-		max-width: 400px;
-
-	}
-
-	.modal .modal-header, .modal .modal-body, .modal .modal-footer {
-
-		padding: 20px 30px;
-
-	}
-
-	.modal .modal-content {
-
-		border-radius: 3px;
-
-	}
-
-	.modal .modal-footer {
-
-		background: #ecf0f1;
-
-		border-radius: 0 0 3px 3px;
-
-	}
-
-	.modal .modal-title {
-
-		display: inline-block;
-
-	}
-
-	.modal .form-control {
-
-		border-radius: 2px;
-
-		box-shadow: none;
-
-		border-color: #dddddd;
-
-	}
-
-	.modal textarea.form-control {
-
-		resize: vertical;
-
-	}
-
-	.modal .btn {
-
-		border-radius: 2px;
-
-		min-width: 100px;
-
-	}
-
-	.modal form label {
-
-		font-weight: normal;
-
-	}
-
-</style>
 
 </head>
 
 <body>
 
-	<!-- Static navbar -->
+    <!-- Static navbar -->
 
-	<nav class="navbar navbar-default">
+    <nav class="navbar navbar-default">
 
-<div class="container-fluid">
+        <div class="container-fluid">
 
-  <div class="navbar-header">
+            <div class="navbar-header">
 
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
 
-      <span class="sr-only">Toggle navigation</span>
+                    <span class="sr-only">Toggle navigation</span>
 
-      <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
 
-      <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
 
-      <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
 
-    </button>
+                </button>
 
-    <a class="navbar-brand" href="#">Fabri Gráfica Digital</a>
+                <a class="navbar-brand" href="#">Fabri Gráfica Digital</a>
 
-  </div>
+            </div>
 
-  <div id="navbar" class="navbar-collapse collapse">
+            <div id="navbar" class="navbar-collapse collapse">
 
-    <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav">
 
-      <li><a href="index.php">INICIO</a></li>
-      <li class="active"><a href="relatorio-detalhado.php">Relatorio detalhado</a></li>
-    </ul>
+                    <li><a href="index.php">INICIO</a></li>
+                    <li class="active"><a href="relatorio-detalhado.php">Relatorio detalhado</a></li>
+                </ul>
 
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><?php echo "$VarNome"; ?></a></li>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><?php echo "$VarNome"; ?></a></li>
 
-      <li><a href="../administrador/logout.php">SAIR</a></li>
+                    <li><a href="../administrador/logout.php">SAIR</a></li>
 
-    </ul>
+                </ul>
 
-  </div><!--/.nav-collapse -->
+            </div>
+            <!--/.nav-collapse -->
 
-</div><!--/.container-fluid -->
+        </div>
+        <!--/.container-fluid -->
 
-</nav>
+    </nav>
 
-	<div class="table-wrapper">
+    <div class="table-wrapper">
 
-		<div class="table-title">
+        <div class="table-title">
 
-			<div class="row">
+            <div class="row">
 
-				<div class="col-sm-6">
-<h5> Serviços concluidos mês <?php echo $month ?></h5>
-				</div>
-				<div class="col-sm-6">
-				
-					<div class="row ">
+                <div class="col-sm-6">
+                    <h5> Serviços concluidos mês <?php echo $month ?></h5>
+                </div>
+                <div class="col-sm-6">
 
-                  
-						<div class="rounded-btn">
-
-							<a href='javascript:window.print()'><button class="btn btn-lg btn-success" target="_blank" ><i class="material-icons">&#xE147;</i>Extrair Relatório</button></a>
-						</div>
-
-					</div>
-
-				</div>
+                    <div class="row ">
 
 
+                        <div class="rounded-btn">
 
-			</div>
+                            <a href='javascript:window.print()'><button class="btn btn-lg btn-success"
+                                    target="_blank"><i class="material-icons">&#xE147;</i>Extrair Relatório</button></a>
+                        </div>
 
-		</div>
+                    </div>
 
-	
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+
         <table class="table table-striped table-hover" id="product_data">
 
-<thead>
+            <thead>
 
-    <tr>
+                <tr>
 
-        <th style="width: 140px;">Data Inicio</th>
-        <th>Solicitante</th>
-        <th>Quantidade</th>
-        <th>Descrição</th>
-        <th>Serviço</th>
-        <th>Atualização</th>
-        <th>Status</th>
-    </tr>
+                    <th style="width: 140px;">Codigo</th>
+                    <th>Solicitante</th>
+                    <th>Quantidade</th>
+                    <th>Serviço</th>
+                    <th>Valor unidade</th>
+                    <th>Total</th>
+                    <th>Status</th>
+                </tr>
 
-</thead>
+            </thead>
 
-<tbody>
+            <tbody>
 
-   <?php
-$result_impres= "SELECT * FROM impressao WHERE MONTH(data) = '$mes' AND YEAR(data) = '$ano' AND impressao.status = '4'";
-$resultado_impres = mysqli_query($conn, $result_impres);
-?>
-<?php while($rows_impres = mysqli_fetch_assoc($resultado_impres)){
-    $status = $rows_impres['status']; 
-    switch ($status) {
-        case '4':
-           $status = "<span>Concluidos</span>";
-            break;
-    }
-    $professor_id = $rows_impres['id_professor'];
-    $produto_id = $rows_impres['id_produto'];
-    $pesquisaprofessor = "SELECT * FROM usuarios WHERE id = '$professor_id' ";
-    $resultado_pesquisa = mysqli_query($conn, $pesquisaprofessor);
-    while($rows_user = mysqli_fetch_assoc($resultado_pesquisa)){ 
-        $pesquisarproduto = "SELECT * FROM produtos WHERE id = '$produto_id'";
-        $resultado_produto = mysqli_query($conn, $pesquisarproduto);
-       
-        while($rows_prod = mysqli_fetch_assoc($resultado_produto)){      
-?>
 
-<tr style="font-size: 12px">
-<td><?php echo $rows_impres['data_inicio']; ?></td>
-<td><?php echo $rows_user['nome']; ?></td>
-<td><?php echo $rows_impres['quantidade']; ?></td>
-<td><?php echo $rows_impres['descricao']; ?></td>
-<td><?php echo $rows_prod['descricao_prod']; ?></td>
-<td><?php echo $rows_impres['data']; ?></td>
-<td><?php echo  $status ?></td>
-  <?php } }}?>
-</tbody>
-</table>
-</div>
 
-</div>
-		<footer>
+                <?php while($rows_impres = mysqli_fetch_assoc($resultado_impres)){
+                 $total_price += $rows_impres['valortotal'];
+	?>
 
-			<div class="footer" id="footer">
+                <tr style="font-size: 12px">
 
-				<div class="container">
+                    <td><?php echo $rows_impres['codigo']; ?></td>
+                    <td><?php echo $rows_impres['nome_professor']; ?></td>
+                    <td><?php echo $rows_impres['totquantidade']; ?></td>
+                    <td><?php echo $rows_impres['descricao_prod']; ?></td>
+                    <td>R$ <?php echo $rows_impres['valor_unidade']; ?></td>
+                    <td> R$ <?php echo $rows_impres['valortotal']; ?></td>
+                    <td><?php echo Status($rows_impres['status']); ?></td>
+                </tr>
 
-					<p class="pull-left"> Copyright © Vedas Sistemas 2019. Todos os direitos reservados. </p>
-				</div>
+                <?php }?>
+            </tbody>
+        </table>
+        <div class="row justify-end">
+            <div class="col-sm-5">
+                <span>Total</span>
+                <h5><?php echo formatCurrency($total_price) ?></h5>
+            </div>
+        </div>
+    </div>
 
-			</div>
+    </div>
+    <footer>
 
-		</div>
+        <div class="footer" id="footer">
 
-	</footer>
+            <div class="container">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+                <p class="pull-left"> Copyright © Vedas Sistemas 2019. Todos os direitos reservados. </p>
+            </div>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        </div>
+
+        </div>
+
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
 
 
 </body>
