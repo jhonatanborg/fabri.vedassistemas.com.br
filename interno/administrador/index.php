d523023536887a5de8d7de8a11f92d288ad5b8de
jhonatanborg <jhonatanborgesdj@gmail.com>
2023-07-25 10:42:32 -0300

    🐛 FIX: title page

--- interno/administrador/index.php
+++ interno/administrador/index.php
@@ -60,7 +60,7 @@ $saldo = getSaldo();
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>SOLICITANTE</title>
+    <title>COORDENADOR</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 
